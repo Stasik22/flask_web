03819141fc63bebcf6aeb0eb48c3061800c405e8
Stasik22 <stanislavmats@icloud.com>
2025-12-03 18:23:28 +0100

    slider v1

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from flask import Flask
-
+from flask import Flask, render_template
 app = Flask(__name__)
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return render_template('index.html')
+
+@app.route('/contact')
+def about():
+    return render_template("contact.html")
+
 
 if __name__ == '__main__':
     app.run(host = "", debug = True)
