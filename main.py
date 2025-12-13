4e12da8a1d23b9bfd12ebb0c23ae1abe86fce549
Stasik22 <macakstas2@gmail.com>
2025-12-13 21:14:01 +0100

    contacts small update

--- main.py
+++ main.py
@@ -11,6 +11,14 @@ def about():
 @app.route("/account")
 def account():
     return render_template("account.html")
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
 
 if __name__ == '__main__':
     app.run(host = "", debug = True)
